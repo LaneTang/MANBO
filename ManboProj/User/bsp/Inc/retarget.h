63aff3792b63229fdeed4abd88b52ba1ed2cf35b
LaneTang <147981118+LaneTang@users.noreply.github.com>
2025-11-04 00:30:42 +0800

    git settings modification

---
+++ ManboProj/User/bsp/Inc/retarget.h
@@ -0,0 +1,11 @@
+//
+// Created by tanga on 2025/10/30.
+//
+
+#ifndef FPV_CTRL_DEMO_RETARGET_H
+#define FPV_CTRL_DEMO_RETARGET_H
+
+#include "usart.h"
+#include <stdio.h>
+
+#endif //FPV_CTRL_DEMO_RETARGET_H
---
+++ ManboProj/User/bsp/Src/retarget.c
@@ -0,0 +1,19 @@
+//
+// Created by tanga on 2025/10/30.
+//
+
+#include "retarget.h"
+#include "usart.h"
+#include <stdio.h>
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return ch;
+}
